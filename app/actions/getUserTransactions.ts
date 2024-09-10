4e22f20ec52ba89d6fbf885153fb24464a35cbde
Libenson Nicolas <libenson.n@gmail.com>
2024-09-10 14:34:21 -0400

    fixed typo

--- app/actions/getUserTransactions.ts
+++ app/actions/getUserTransactions.ts
@@ -4,28 +4,31 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { Transaction } from "@/types/Transaction";
 
-export const getUserTransaction = async (): Promise<{
-  transactions: Transaction[];
+type GetUserTransactionProps = {
+  transactions?: Transaction[];
   error?: string;
-}> => {
-  const { userId } = auth();
-
-  if (!userId) {
-    return { error: "User not found" };
-  }
-
-  try {
-    const transactions = await db.transaction.findMany({
-      where: { userId },
-      orderBy: {
-        createdAT: "desc",
-      },
-    });
-
-    return { transactions };
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "Database error";
-    return { error: errorMessage };
-  }
 };
+
+export const getUserTransaction =
+  async (): Promise<GetUserTransactionProps> => {
+    const { userId } = auth();
+
+    if (!userId) {
+      return { error: "User not found" };
+    }
+
+    try {
+      const transactions = await db.transaction.findMany({
+        where: { userId },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      return { transactions };
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "Database error";
+      return { error: errorMessage };
+    }
+  };
